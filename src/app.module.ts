ddf283ca300c0bfb571d8f21fc3cb9d1369f064c
Chandrakant <Chandrakant@themetromaxgroup.com>
2023-08-01 14:02:01 +0530

    Config Service Added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,41 @@
-import { OptionModule } from './modules/option/option.module';
-import { QuestionModule } from './modules/question/question.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuestionModule } from './modules/question/question.module';
 import { QuizModule } from './modules/quiz/quiz.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
+import { OptionModule } from './modules/option/option.module';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [QuestionModule, QuizModule, OptionModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          // envFilePath default path is .env but we can provide path from here
+        }),
+      ],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        // Do not use synchronize in production you will loose your data
+        synchronize: true,
+        // It will show all the query in console
+        logging: true,
+      }),
+    }),
+    QuizModule,
+    OptionModule,
+    QuestionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
